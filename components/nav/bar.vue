37f4b7a879f0eab055938ea19fe962a0577d0e14
Brian-mula <mulatiwangilabrian@gmail.com>
2023-06-10 00:02:13 +0300

    added logo

--- components/nav/bar.vue
+++ components/nav/bar.vue
@@ -1,8 +1,10 @@
 <template>
     <div>
-       <div class="navbar bg-base-100">
+       <div class="navbar bg-base-100/[.06]">
       <div class="flex-1">
-        <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+        <div class="h-16 w-16 rounded-full mx-4">
+            <img src="assets/images/logo.jpeg" alt="" class="h-full w-full rounded-full">
+        </div>
       </div>
       <div class="flex-none">
         <ul class="menu menu-horizontal px-1">
@@ -25,6 +27,7 @@
 </template>
 
 <script setup lang="ts">
+
 
 </script>
 

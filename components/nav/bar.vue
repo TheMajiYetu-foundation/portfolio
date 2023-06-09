8e880a9111cb4a797bfc2730ab5ad8f6e189228a
Brian-mula <mulatiwangilabrian@gmail.com>
2023-06-09 23:31:15 +0300

    added sidebar

---
+++ components/nav/bar.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        Top nav
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/nav/side.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        Side nav
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>

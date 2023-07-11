bca32ed315e78bd67dc00f071323e03b17add955
Brian-mula <mulatiwangilabrian@gmail.com>
2023-07-11 15:48:05 +0300

    added hero section

---
+++ components/sections/hero.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div class="flex">
+           <div class="px-10 py-10">
+             <h3 class="text-6xl tracking-wide uppercase text-white">Climate action</h3>
+            <h3 class="text-6xl tracking-wide text-white">against drought</h3>
+           </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>

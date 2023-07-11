de202079febf284606370cd17ded2283f3b416d1
Brian-mula <mulatiwangilabrian@gmail.com>
2023-07-11 16:46:47 +0300

    wrapped up

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,14 @@
 <template>
     <div>
         <SectionsHero/>
+        <div class="pt-10">
+            <h3 class="text-4xl text-center text-white">Our Team</h3>
+        </div>
+        <div class="grid grid-cols-3 gap-x-20 gap-y-16 px-6 py-6">
+            <div v-for="person in teams" :key="person.name">
+                <UserCard :name="person.name" :description="person.description" />
+            </div>
+        </div>
     </div>
 </template>
 
@@ -11,6 +19,21 @@ definePageMeta({
     title: 'landing'
 
 });
+
+const teams = ref([
+    {
+        name:'Grace Muthoni',
+        description:'Full stack developer, Cloud practitioner,Data analyst'
+    },
+    {
+        name:'Mulati Brian',
+        description:'Full stack developer, Cloud practitioner,Data analyst'
+    },
+    {
+        name:'Grace Muthoni',
+        description:'Full stack developer, Cloud practitioner,Data analyst'
+    }
+])
 </script>
 
 <style scoped></style>
